51e8d6809ba2d5e2c25e68760b3d27f96ef38134
ak14092003 <arpitdav4444@gmail.com>
2024-05-23 16:17:07 +0530

    account update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 // code to find the local host page below
 app.get("/",(req, res) => {
     //console.log(req.rawHeaders); // raw headers are list of key value pairs from where the req came from
-    res.send("<h1>Hello World</h1>");//send response to server(local host 3000)
+    res.send("<h1>Hello World I am Arpit Kumar</h1>");//send response to server(local host 3000)
 });
 
 app.get("/about", (req, res) => {
